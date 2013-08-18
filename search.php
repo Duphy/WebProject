61ed6f0597c485e50beadfec73b38f1a7825eca3
zhixing <zhixing135@hotmail.com>
2013-08-18 11:02:55 +0800

    Modified search function

--- search.php
+++ search.php
@@ -8,10 +8,11 @@
 
 	<div class="search_field">
 		<div class="search_bar">
-			<form action="database/backend_search.php" method="post"
+			<form action="search.php" method="post"
 				enctype="multipart/form-datd">
 				<input type="text" name="search_content" placeholder="search" /><br />
-				<input name="search_button" id="search_button" type="submit" />
+				<input name="isSubmitted" id="isSubmitted" type="hidden" value = '1'/> 
+				<input name="search_button" id="search_button" type="submit" value='Submit'/>
 			
 		<div class="search_option">
 			<input type="checkbox" name="search_people" value= "people" checked /> &nbsp;&nbsp;People&nbsp;&nbsp;
@@ -46,7 +47,93 @@
 		</div>
 	</div>
 	
-	<?php include_once 'sidebar.php';?>
+	<?php 
+
+		include_once 'sidebar.php';
+		
+		if (isset($_POST['isSubmitted']) && $_POST['isSubmitted'] == 1){
+			//search($currentUserId, $search_type, $mode, $keys,$session_key, $location=-1, $option = -1,  $age = -1, $gender=-1)
+	
+			$flag_all =false;
+		    $mode = $_POST["search_mode"];
+		    
+			if ((isset($_POST["match_option_name"]) && isset($_POST["match_option_tag"])) ||
+				(isset($_POST["match_option_user"]) && isset($_POST["match_option_post"]))){
+				$option = 0;
+			}
+			else if(isset($_POST["match_option_name"]) || isset($_POST["match_option_user"])){
+				$option = 1;
+			}
+			else if(isset($_POST["match_option_tag"]) || isset($_POST["match_option_post"])){
+				$option = 2;
+			}
+			else 
+				$option =-1;
+			
+			if (isset($_POST["gender_male"]) && isset($_POST["gender_female"])){
+				$gender = 2;
+			}
+			else if(isset($_POST["gender_male"])){
+				$gender = 1;
+			}
+			else if(isset($_POST["gender_female"])){
+				$gender = 0;
+			}
+			else if(isset($_POST["gender_other"])){
+				$gender = 3;
+			}
+
+			$location = $_POST["match_location"];
+			if(isset($_POST["search_people"])){
+				$search_type = 0;
+				$request = search($_SESSION['uid'], $search_type, 
+							$mode, $_POST['search_content'],$_SESSION['session_key'], 
+							$location, $option, $_POST['age_lower_bound'],$_POST['age_upper_bound'], $gender);
+			}
+			if(isset($_POST["search_event"])){
+				$search_type = 1;
+				$request = search($_SESSION['uid'], $search_type,
+						$mode, $_POST['search_content'],$_SESSION['session_key'],
+						$location,$option);
+			}
+			if(isset($_POST["search_post"])){
+				$search_type = 2;
+				$request = search($_SESSION['uid'], $search_type,
+						$mode, $_POST['search_content'],$_SESSION['session_key'],
+						$location,$option);
+			}
+			//print_request($request);
+			//die();
+			$response = connect_to_server_and_send_message($request);
+			//print_byte_array($response,25);
+			//print_request($response);
+			$pkg = unpack_pkg($response);
+			print_array($pkg);
+			
+				
+		}
+	?>
+
+
+		<div class="searchresults">
+
+			<div id="profile photo">
+				<img
+					src="https://lh3.googleusercontent.com/-QzyS5Mtzh2s/AAAAAAAAAAI/AAAAAAAAAAA/u9rVtEolGdU/s46-c-k-no/photo.jpg">
+			</div>
+
+			<div id="name">Ben Leong</div>
+
+			<div id="tags">Computer | Japanese Food | Sweet Stuff | Photography</div>
+
+			<div id="location">Singapore</div>
+
+			<div id="add_friend">
+			
+			<a href="">Add Friend</a>
+		</div>
+
+	</div>
 </body>
 
 <footer> </footer>
