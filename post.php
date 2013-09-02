2480e89e918ea5d670d68f4a911269da8e4eef2a
Duphy <dyzhwx@gmail.com>
2013-09-02 21:36:35 +0800

    update

--- post.php
+++ post.php
@@ -5,6 +5,22 @@
 	
 	$tags = array("Love");
 	//$tags = array();
-	$response = connect_create_post(0, 'LOVELOVELOVELOVELOVE', 0, $tags);
+	$response = connect_create_post(0, 'LOVELOVELOVELOVE', 0, $tags);
 	print_array($response);
+	//$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+	if($response[1][0]==1){
+		$arr = array(
+			'success' => false,
+			'reason' => $response[1][1][0]
+		);
+	}
+	else{
+		$arr = array(
+			'success' => true,
+			'uid' => $response[1][1][0],
+			'eid' => $response[1][1][1],
+			'pid' => $response[1][1][2]
+		);
+	}
+	echo json_encode($arr);
 ?>
